5728f1bb6ff38915b3be91b9a45f64f6587fe368
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-05-22 23:32:42 -0400

    Console app minor cleanup

--- console-app/src/processor.spec.ts
+++ console-app/src/processor.spec.ts
@@ -1,11 +1,12 @@
 import "mocha";
+
 import { expect } from "chai";
+
 import { commandProcesor } from "./processor";
-import { exec } from "child_process";
 
 describe("Command Parser", () => {
   it("should process a command", () => {
     const output = commandProcesor({ accountNumber: 12345, url: "http://google.com"});
     expect(output).to.be.equal(12345);
-  })
+  });
 });
